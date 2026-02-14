3ec05edb373badfbffffc6181748c17f2b4699ad
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2026-02-14 17:16:58 +0600

    file explorer runnign with create file and folder with  react symbol :

--- convex/files.ts
+++ convex/files.ts
@@ -120,14 +120,18 @@ export const createFile = mutation({
 
     if (existing) throw new Error("File already exists!");
 
+    const now = Date.now()
     await ctx.db.insert("files", {
       projectId: args.projectId,
       name: args.name,
       content: args.content,
       type: "file",
       parentId: args.parentId,
-      updatedAt: Date.now(),
+      updatedAt: now,
     });
+    await ctx.db.patch("projects",args.projectId,{
+      updatedAt:now
+    })
   },
 });
 export const createFolder = mutation({
@@ -162,14 +166,19 @@ export const createFolder = mutation({
 
     if (existing) throw new Error("Folder already exists!");
 
+    const now = Date.now()
     await ctx.db.insert("files", {
       projectId: args.projectId,
       name: args.name,
 
       type: "folder",
       parentId: args.parentId,
-      updatedAt: Date.now(),
+      updatedAt: now,
     });
+
+    await ctx.db.patch("projects",args.projectId,{
+      updatedAt:now
+    })
   },
 });
 
@@ -211,9 +220,14 @@ export const renameFile = mutation({
         )
     }
 
+    const now = Date.now()
     await ctx.db.patch("files",args.id,{
         name:args.newName,
-        updatedAt:Date.now()
+        updatedAt:now
+    })
+
+    await ctx.db.patch("projects",file.projectId,{
+      updatedAt:now
     })
   },
 });
@@ -241,7 +255,68 @@ export const deleteFile = mutation({
     // recursively delete file/fodler and all descendants
 
     const deleteRecursive  = async(fileId: Id<"files"> ) =>{
-        
+        const item = await ctx.db.get("files",fileId)
+
+        if(!item){
+          return;
+        }
+
+        // if it is a folder then delete all children 1st
+        if(item.type === "folder"){
+          const children = await ctx.db.query("files").withIndex("by_project_parent",(q)=>q.eq("projectId",item.projectId).eq("parentId",fileId))
+          .collect()
+
+          for(const child of children){
+            await deleteRecursive(child._id)
+          }
+        }
+        // delete storage file if it is exists
+        if(item.storageId){
+          await ctx.storage.delete(item.storageId);
+        }
+
+        // delete the file / folder itself
+        await ctx.db.delete("files",fileId)
     }
+    await deleteRecursive(args.id)
+
+    await ctx.db.patch("projects",file.projectId,{
+      updatedAt:Date.now()
+    })
   },
 });
+
+export const updateFile = mutation({
+  args:{
+    id:v.id("files"),
+    content:v.string(),
+  },
+  handler: async(ctx,args)=>{
+    const identity = await verifyAuth(ctx);
+    const file = await ctx.db.get("files", args.id);
+
+    if (!file) throw new Error("File not found!");
+
+    const project = await ctx.db.get("projects", file.projectId);
+
+    if (!project) {
+      throw new Error("Project not found!");
+    }
+
+    if (project.ownerId !== identity.subject) {
+      throw new Error("Unauthorized access to this project.");
+    }
+
+    const now = Date.now()
+
+    await ctx.db.patch("files",args.id,{
+      content:args.content,
+      updatedAt:now
+    })
+
+
+    await ctx.db.patch("projects",file.projectId,{
+      updatedAt:Date.now()
+    })
+  }
+})
