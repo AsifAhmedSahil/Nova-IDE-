48493989696756586e8c97325fea9aee435fbd46
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2026-02-25 23:28:38 +0600

    add extensions:

---
+++ src/features/editor/extensions/language-extension.ts
@@ -0,0 +1,8 @@
+import {Extension} from "@codemirror/state"
+import {javascript} from "@codemirror/lang-javascript"
+import {html} from "@codemirror/lang-html" 
+import {css} from "@codemirror/lang-css" 
+import {json} from "@codemirror/lang-json" 
+import {markdown} from "@codemirror/lang-markdown" 
+import {python} from "@codemirror/lang-python" 
+
